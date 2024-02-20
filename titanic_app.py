cd010c4be0833b04cf9b31d5805404f429431f5b
Rafael Takata <147168621+RafaelTakata0105@users.noreply.github.com>
2024-02-20 09:43:00 -0600

    Update titanic_app.py

--- titanic_app.py
+++ titanic_app.py
@@ -2,23 +2,23 @@ import streamlit as st
 import pandas as pd
 import matplotlib as plt
 import numpy as np
-import altair as alt
+
 path = "train.data"
 df = pd.read_csv(path, index_col= 'PassengerId')
 st.title("Streamit Histogramas Titanic")
 st.header("Rafael Takata García")
 st.dataframe(df.head(3))
 st.markdown('## Histogramas por features') 
-st.altair_chart(df.hist(figsize=[7,7], xlabelsize=5))
+st.write(df.hist(figsize=[7,7], xlabelsize=5))
 st.markdown('### Survived')
-st.altair_chart(df['Survived'].plot(kind='hist', bins = 2, edgecolor = "BLACK"))
+st.write(df['Survived'].plot(kind='hist', bins = 2, edgecolor = "BLACK"))
 st.markdown('### Passenger class')
-st.altair_chart(df['Pclass'].plot(kind='hist', bins = 3, color="#F2AB6D", edgecolor = "BLACK"))
+st.write(df['Pclass'].plot(kind='hist', bins = 3, color="#F2AB6D", edgecolor = "BLACK"))
 st.markdown('### Age')
-st.altair_chart(df['Age'].plot(kind='hist', edgecolor = "WHITE", bins= 8))
+st.write(df['Age'].plot(kind='hist', edgecolor = "WHITE", bins= 8))
 st.markdown('### Siblings/Spouses aboard')
-st.altair_chart(df['SibSp'].plot(kind='hist', legend=True, bins=8))
+st.write(df['SibSp'].plot(kind='hist', legend=True, bins=8))
 st.markdown('### Parents/children aboard')
-st.altair_chart(df['Parch'].plot(kind='hist', grid= True, xlabel="Parents / children aboard", bins = 6))
+st.write(df['Parch'].plot(kind='hist', grid= True, xlabel="Parents / children aboard", bins = 6))
 st.markdown('### Passenger fare')
-st.altair_chart(df['Fare'].plot(kind='hist', grid=True, edgecolor="BLACK", bins = 5))
+st.write(df['Fare'].plot(kind='hist', grid=True, edgecolor="BLACK", bins = 5))
